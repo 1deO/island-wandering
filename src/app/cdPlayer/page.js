27a1226b7a57a2d4e64612d26f6ad0de78b0a189
1deO <b0970731765@gmail.com>
2025-06-15 15:16:52 +0800

    Update page.js

--- src/app/cdPlayer/page.js
+++ src/app/cdPlayer/page.js
@@ -73,7 +73,7 @@ export default function CdPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [coverPosition, setCoverPosition] = useState({ x: '45%', y: '90%' });
   const [currentArea, setCurrentArea] = useState('taiwan');
-
+//test
   const [showYoutubePlayer, setShowYoutubePlayer] = useState(false);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 

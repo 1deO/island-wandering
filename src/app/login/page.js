7daee921615e0bc409c4b7c91036806626b1f354
1deO <b0970731765@gmail.com>
2025-06-16 15:00:17 +0800

    up

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import GoogleAuth from "../component/GoogleAuth";
+import GoogleAuth from "../component/GoogleAuth.js";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import Link from "next/link";
